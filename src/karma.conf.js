b897c460582f68ded452b5d254943caa0f8e2044
Diptesh <dipt912@gmail.com>
2020-01-28 09:49:15 -0800

    Undo phantomjs

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-chrome-launcher'),
+      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -26,19 +26,19 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     // updated
-    browsers: ['ChromeHeadless'],
-    // new
-    customLaunchers: {
-      'ChromeHeadless': {
-        base: 'Chrome',
-        flags: [
-          '--no-sandbox',
-          '--headless',
-          '--disable-gpu',
-          '--remote-debugging-port=9222'
-        ]
-      }
-    },
+    browsers: ['PhantomJS'],
+    // // new
+    // customLaunchers: {
+    //   'ChromeHeadless': {
+    //     base: 'Chrome',
+    //     flags: [
+    //       '--no-sandbox',
+    //       '--headless',
+    //       '--disable-gpu',
+    //       '--remote-debugging-port=9222'
+    //     ]
+    //   }
+    // },
     singleRun: true,
     restartOnFileChange: true
   });
