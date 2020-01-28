6ccd6772d29497b5ca2e4f844654b053e84e83da
Diptesh <dipt912@gmail.com>
2020-01-27 23:11:56 -0800

    Chrome added

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -7,39 +7,39 @@ module.exports = function (config) {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
-      require('karma-phantomjs-launcher'),
+      require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma'),
-      require('karma-verbose-reporter')
+      require('@angular-devkit/build-angular/plugins/karma')
     ],
     client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
+      clearContext: false
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage'),
+      dir: require('path').join(__dirname, '../coverage/example'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    reporters: ['progress', 'kjhtml', 'verbose'],
+    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    // customLaunchers: {
-    //   ChromeHeadless: {
-    //     base: 'Chrome',
-    //     flags: [
-    //       '--headless',
-    //       '--disable-gpu',
-    //       '--no-sandbox',
-    //       '--remote-debugging-port=9222',
-    //     ]
-    //   }
-    // },
-    browsers: [
-      'PhantomJS',
-    ],
-    singleRun: true
+    // updated
+    browsers: ['ChromeHeadless'],
+    // new
+    customLaunchers: {
+      'ChromeHeadless': {
+        base: 'Chrome',
+        flags: [
+          '--no-sandbox',
+          '--headless',
+          '--disable-gpu',
+          '--remote-debugging-port=9222'
+        ]
+      }
+    },
+    singleRun: true,
+    restartOnFileChange: true
   });
 };
